fac7941ec9503a28c85b15f24c825d135104b96a
linpeiqin <286513319@qq.com>
2020-10-22 09:28:03 +0800

    1、bug修复：修复根节点新增菜单保存失败的bug；

--- src/main/resources/static/user/js/shortcutmenu.js
+++ src/main/resources/static/user/js/shortcutmenu.js
@@ -69,6 +69,9 @@ layui.use(['element', 'form', 'table', 'layer', 'tree', 'util'], function () {
  */
 function shortcutMenuFormSave() {
     let shortcutMenuForm = $("#shortcutMenuForm").serializeObject();
+    if (shortcutMenuForm.shortcutMenuId === "") {
+        return;
+    }
     if (shortcutMenuForm.shortcutMenuParentId === "") {
         layer.msg("根节点仅用于展示，不可操作！", {icon: 2, time: 2000}, function () {
         });
